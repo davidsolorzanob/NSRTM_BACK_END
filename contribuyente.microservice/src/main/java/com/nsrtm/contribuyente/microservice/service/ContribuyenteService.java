93f69c60594b79dc281974fbfca53fd2ffa1b4b1
Luis Espiritu <luisespiritu1@gmail.com>
2022-04-27 01:28:48 -0500

    Ajustes en filtros del contribuyente

--- contribuyente.microservice/src/main/java/com/nsrtm/contribuyente/microservice/service/ContribuyenteService.java
+++ contribuyente.microservice/src/main/java/com/nsrtm/contribuyente/microservice/service/ContribuyenteService.java
@@ -1,12 +1,14 @@
 package com.nsrtm.contribuyente.microservice.service;
 
 import java.util.List;
+import java.util.Locale;
 
 import com.nsrtm.contribuyente.microservice.domain.Domicilio;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import com.nsrtm.contribuyente.microservice.domain.Contribuyente;
@@ -82,16 +84,40 @@ public class ContribuyenteService {
 		//Specification<Contribuyente> spec =
 		//return contribuyenteRepository.findByTipoDocumentoAndNumeroDocumentoAndSecEjec(e.tipoDocumento, e.numeroDocumento, e.secEjec);
 	//}
-	private static Specification<Contribuyente> getByNroDocumento(String numeroDocumento) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("numeroDocumento"), numeroDocumento);
+
+	private static Specification<Contribuyente> getByApellidoPaterno(String apellidoPaterno) {
+		if(apellidoPaterno == null) return null;
+		if(apellidoPaterno.isEmpty() || apellidoPaterno.isBlank()) return null;
+		return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.upper(root.get("apellidoPaterno")), "%" + apellidoPaterno.toUpperCase(Locale.ROOT) + "%");
 	}
 
-	private static Specification<Contribuyente> getBySecEjec(String secEjec) {
-		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("secEjec"), secEjec);
+	private static Specification<Contribuyente> getByApellidoMaterno(String apellidoMaterno) {
+		if(apellidoMaterno == null) return null;
+		if(apellidoMaterno.isEmpty() || apellidoMaterno.isBlank()) return null;
+		return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.upper(root.get("apellidoMaterno")), "%" + apellidoMaterno.toUpperCase(Locale.ROOT) + "%");
+	}
+
+	private static Specification<Contribuyente> getByNombres(String nombres) {
+		if(nombres == null) return null;
+		if(nombres.isEmpty() || nombres.isBlank()) return null;
+		return (root, query, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.upper(root.get("nombres")),"%" + nombres.toUpperCase(Locale.ROOT) + "%");
+	}
+
+	private static Specification<Contribuyente> getByCodigo(String codigo) {
+		if(codigo == null) return null;
+		if(codigo.isEmpty() || codigo.isBlank()) return null;
+		return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("codigo"), codigo);
 	}
 
 	public List<Contribuyente> ListarPorFiltros(Contribuyente e){
-		Specification<Contribuyente> spec = Specification.where(getByNroDocumento(e.numeroDocumento).and(getBySecEjec(e.secEjec)));
+		Specification<Contribuyente> spec = null;
+		Specification<Contribuyente> specTemp = getByApellidoPaterno(e.apellidoPaterno);
+		specTemp = specTemp==null ? getByApellidoMaterno(e.apellidoMaterno):specTemp.and(getByApellidoMaterno(e.apellidoMaterno));
+		specTemp = specTemp==null ? getByNombres(e.nombres):specTemp.and(getByNombres(e.nombres));
+		specTemp = specTemp==null ? getByCodigo(e.codigo):specTemp.and(getByCodigo(e.codigo));
+		spec = Specification.where(specTemp);
+		if(spec == null)
+			return contribuyenteRepository.findAll();
 		return contribuyenteRepository.findAll(spec);
 	}
 }
--- maestro.microservice/src/main/java/com/nsrtm/maestro/microservice/Application.java
+++ maestro.microservice/src/main/java/com/nsrtm/maestro/microservice/Application.java
@@ -2,6 +2,11 @@ package com.nsrtm.maestro.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +15,23 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<?, ?> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		return template;
+	}
+
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig() //
+				.prefixCacheNameWith(this.getClass().getPackageName() + ".") ;//
+		//.entryTtl(Duration.ofHours(1)) //
+		//.disableCachingNullValues();
+
+		return RedisCacheManager.builder(connectionFactory) //
+				.cacheDefaults(config) //
+				.build();
+	}
 }

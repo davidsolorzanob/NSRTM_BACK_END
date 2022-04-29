c4c6c26d7d329c21f460babef559afcf7dae72fa
Luis Espiritu <luisespiritu1@gmail.com>
2022-04-29 11:48:19 -0500

    Habilitación de CORS para acceso de cliente

--- maestro.microservice/src/main/java/com/nsrtm/maestro/microservice/Application.java
+++ maestro.microservice/src/main/java/com/nsrtm/maestro/microservice/Application.java
@@ -1,5 +1,7 @@
 package com.nsrtm.maestro.microservice;
 
+import com.nsrtm.maestro.microservice.config.CORS;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,14 +9,54 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
+	@Autowired
+	private CORS cors;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+/*
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping(configCORS.getMapping())
+						.allowedOrigins(configCORS.getAllowOrigins())
+						.allowedMethods(configCORS.getAllowMethods())
+						//.allowCredentials(configCORS.isAllowCredentials())
+						.maxAge(configCORS.getMaxAge());
+						//.exposedHeaders(configCORS.getExposedHeaders());
+			}
+		};
+	}
+*/
+	@Bean
+	CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(cors.allowCredentials); // you USUALLY want this
+		config.setAllowedOrigins(Arrays.asList(cors.allowOrigins.split(",")));
+		config.setAllowedHeaders(Arrays.asList(cors.allowHeaders.split(",")));
+		config.setAllowedMethods(Arrays.asList(cors.allowMethods.split(",")));
+		config.setMaxAge(Duration.ofSeconds(cors.getMaxAge()));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration(cors.getMapping(), config);
+
+		return new CorsFilter(source);
 	}
 
 	@Bean

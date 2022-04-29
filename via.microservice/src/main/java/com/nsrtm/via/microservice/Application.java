674e155f304548fbd9004e8d65ff6a3504042339
Luis Espiritu <luisespiritu1@gmail.com>
2022-04-29 11:59:49 -0500

    Implementación de Redis - Inicial

--- via.microservice/src/main/java/com/nsrtm/via/microservice/Application.java
+++ via.microservice/src/main/java/com/nsrtm/via/microservice/Application.java
@@ -1,5 +1,7 @@
 package com.nsrtm.via.microservice;
 
+import com.nsrtm.via.microservice.config.CORS;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,12 +10,19 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.time.Duration;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableCaching
 public class Application {
+
+	@Autowired
+	private CORS cors;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -25,6 +34,21 @@ public class Application {
 		template.setConnectionFactory(connectionFactory);
 
 		return template;
+	}
+
+	@Bean
+	CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(cors.allowCredentials); // you USUALLY want this
+		config.setAllowedOrigins(Arrays.asList(cors.allowOrigins.split(",")));
+		config.setAllowedHeaders(Arrays.asList(cors.allowHeaders.split(",")));
+		config.setAllowedMethods(Arrays.asList(cors.allowMethods.split(",")));
+		config.setMaxAge(Duration.ofSeconds(cors.getMaxAge()));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration(cors.getMapping(), config);
+
+		return new CorsFilter(source);
 	}
 
 	@Bean

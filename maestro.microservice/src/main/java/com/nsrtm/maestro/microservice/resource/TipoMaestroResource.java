60695cec2ec36e2f59d9422841d8b77f8c999305
Luis Espiritu <luisespiritu1@gmail.com>
2022-05-03 16:03:46 -0500

    Ajustes de CORS en microservio Maestro y Tipo Maestro

--- maestro.microservice/src/main/java/com/nsrtm/maestro/microservice/resource/TipoMaestroResource.java
+++ maestro.microservice/src/main/java/com/nsrtm/maestro/microservice/resource/TipoMaestroResource.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.nsrtm.maestro.microservice.domain.TipoMaestro;
 import com.nsrtm.maestro.microservice.service.TipoMaestroService;

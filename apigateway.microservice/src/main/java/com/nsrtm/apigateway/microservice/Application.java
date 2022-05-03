874fc17baeab2932fc2e379966ca8a386c51b27c
Luis Espiritu <luisespiritu1@gmail.com>
2022-05-03 12:22:38 -0500

    configuración del api gateway

--- apigateway.microservice/src/main/java/com/nsrtm/apigateway/microservice/Application.java
+++ apigateway.microservice/src/main/java/com/nsrtm/apigateway/microservice/Application.java
@@ -21,7 +21,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+/*
 	@Bean
 	CorsFilter corsFilter() {
 		CorsConfiguration config = new CorsConfiguration();
@@ -36,4 +36,5 @@ public class Application {
 
 		return new CorsFilter(source);
 	}
+*/
 }
